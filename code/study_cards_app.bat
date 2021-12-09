6c644955d54b3dcee49e2859f34c6e9556d12073
apex956 <89335960+apex956@users.noreply.github.com>
2021-12-09 14:58:17 +0200

    Added a windows batch file

---
+++ code/study_cards_app.bat
@@ -0,0 +1,5 @@
+@echo off
+rem this batch file runs the Study Cards application on a windows platform
+"C:\Users\apex\AppData\Local\Programs\Python\Python39-32\python.exe" "study_cards_app.py"
+rem pause
+
